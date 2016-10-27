e365b08b76ae760f20dd45f8b716320059714da3
andrew-h-lee <mr.ahlee@gmail.com>
2016-10-28 01:18:27 +1100

    render dynamic HTML; add dangerouslySetInnerHTML

--- app/Card.js
+++ app/Card.js
@@ -19,7 +19,8 @@ class Card extends Component {
     if (this.state.showDetails) {
       cardDetails = (
         <div className="card__details">
-          {marked(this.props.description)}
+          <span dangerouslySetInnerHTML=
+            {{__html:marked(this.props.description)}} />
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       );
