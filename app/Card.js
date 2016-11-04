1daca6bf5ea1408ae3ab1d25e84db262c076edf3
andrew-h-lee <mr.ahlee@gmail.com>
2016-11-05 01:33:30 +1100

    Fix Error; should be less than

--- app/Card.js
+++ app/Card.js
@@ -5,7 +5,7 @@ import marked from 'marked';
 let titlePropType = (props, propName, componentName) => {
   if (props[propName]) {
     let value = props[propName];
-    if (typeof value !== 'string' || value.length > 80) {
+    if (typeof value !== 'string' || value.length < 80) {
       return new Error(
         `${propName} in ${componentName} is longer than 80 characters`
       );
